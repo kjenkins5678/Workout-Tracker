403c44fb9624b410a7473773bb1d5bca34d32b3a
kjenkins5678 <kajenkins9@gmail.com>
2020-03-23 16:32:14 -0400

    ui changes to api links

--- public/index.js
+++ public/index.js
@@ -11,13 +11,13 @@ fetch("/api/workouts")
       div.innerHTML = `
       <div class="col-lg-3 col-md-6 mb-4">
         <div class="card h-100">
-          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
           <div class="card-body">
             <h4 class="card-title">${workout.name}</h4>
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
           </div>
           <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
+            <a href="#" class="btn btn-primary">Log</a>
+            <a href="#" class="btn btn-primary">Edit</a>
           </div>
         </div>
       </div>
