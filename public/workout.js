270807995e5227afd1cc5397a48fe12e2fe4addc
kjenkins5678 <kajenkins9@gmail.com>
2020-03-31 18:14:15 -0400

    add exercises to page without needing refresh

--- public/workout.js
+++ public/workout.js
@@ -10,54 +10,71 @@ $(document).ready(function() {
     workoutId = url.split("=")[1];
   }
 
-  function GetUserData() {
-    return userObject = {
-      name : nameInput.val(),
-      duration : durInput.val(),
-      calories : caloriesInput.val()
-    };
-  };
-
   // Get exercises from api and display them on the page
 
-  fetch(`/api/workouts/${workoutId}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
+  function clearForm() {
+    form.empty();
+  }
 
-      //Select workout-name id
-      const workoutHeading = document.querySelector("#workout-name");
-      //Add the workout name from the db as the title of the page
-      workoutHeading.innerHTML = data.name;
+  function drawPage() {
 
-      const exercisesSpot = document.querySelector("#exercises");
+    fetch(`/api/workouts/${workoutId}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
 
-      //For each exercise...
-      data.exercises.forEach(exercise => {
-        console.log(exercise.name, exercise.duration, exercise.calories);
+        //Select workout-name id
+        const workoutHeading = document.querySelector("#workout-name");
+        //Add the workout name from the db as the title of the page
+        workoutHeading.innerHTML = data.name;
 
-        const div = document.createElement("div");
-        div.classList.add("form-row");
+        const exercisesSpot = document.querySelector("#exercises");
 
+        //For each exercise...
+        data.exercises.forEach(exercise => {
+          console.log(exercise.name, exercise.duration, exercise.calories);
+
+          const div = document.createElement("div");
+          div.classList.add("form-row");
+
+          //html template for the values in the database
+          div.innerHTML = `
+            <div class="col-7">
+              <input type="text" class="form-control" value="${exercise.name}" id="exercise-name-${exercise._id}">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" value="${exercise.duration}" id="exercise-duration-${exercise._id}">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" value="${exercise.calories}" id="exercise-calories-${exercise._id}">
+          </div>
+            <div class="col-auto">
+              <button type="delete" class="btn btn-danger mb-2" id="exercise-${exercise._id}">Remove</button>
+            </div>
+          `;
+          exercisesSpot.append(div);
+        });
+
+        //html template for the input form fields
         div.innerHTML = `
-          <div class="col-7">
-            <input type="text" class="form-control" placeholder="${exercise.name}" id="exercise-name">
+        <div class="col-7">
+              <input type="text" class="form-control" placeholder="Name" id="exercise-name">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Duration" id="exercise-duration">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Calories" id="exercise-calories">
           </div>
-          <div class="col">
-            <input type="text" class="form-control" placeholder="${exercise.duration}" id="exercise-duration">
-          </div>
-          <div class="col">
-            <input type="text" class="form-control" placeholder="${exercise.calories}" id="exercise-calories">
-         </div>
-          <div class="col-auto">
-            <button type="delete" class="btn btn-danger mb-2">Remove</button>
-           </div>
-        `;
-        exercisesSpot.prepend(div);
-
+            <div class="col-auto">
+              <button type="submit" class="btn btn-primary mb-2" id="add">Add</button>
+            </div>
+        `
       });
-    });
+  }
+
+  drawPage();
 
   form.submit((e) => {
     e.preventDefault();
@@ -80,7 +97,9 @@ $(document).ready(function() {
     fetch(`/api/new/exercise/${workoutId}`, requestOptions)
       .then(response => response.text())
       .then(result => {
-        console.log(result)
+        console.log(result);
+        clearForm();
+        drawPage();
       })
       .catch(error => console.log("error", error));
   });
